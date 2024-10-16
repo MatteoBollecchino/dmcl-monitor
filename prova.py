c103bc81696fe29356bb5a79c8852b61f9cb4da0
MatteoBollecchino <matthbolle@gmail.com>
2024-10-16 19:36:32 +0200

    Aggiornamento Codice

--- prova.py
+++ prova.py
@@ -1,2 +1,19 @@
+# Progetto di Prova per imparare pyton
+# La funzione 'in' restituisce un booleano per la presenza o meno della sottostringa indicata a sinistra in quella a destra
 
-print("Hello Wolrd!")
+print("Hello"+" Wolrd!")
+print(80/3)
+
+name = "Matteo"
+surname = "Bollecchino"
+
+print("My name is "+ name + " " +surname)
+print("M" in name)
+
+def my_first_function():
+    print("My first function")
+
+def dummy_function():
+    pass
+
+my_first_function()
