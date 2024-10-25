6ac07c4b199dedd5410b573714bd8378de179725
MatteoBollecchino <matthbolle@gmail.com>
2024-10-25 15:06:34 +0200

    Aggiornamento

---
+++ supervised_example.py
@@ -0,0 +1,21 @@
+import pandas
+
+
+# Main Function
+first_time = True
+if __name__ == "__main__":
+    
+    # 0) Load Dataset (Library: PANDAS / NUMPY)
+
+    my_dataset = pandas.read_csv("./labelled_dataset.csv")
+    a = 1
+
+    # 1) Split Dataset
+
+    # 2) Choose Classifier (Library: SCIKIT LEARN)
+
+    # 3) Train Classifier
+
+    # 4) Test Classifier
+
+    pass
